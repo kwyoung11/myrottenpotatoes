802134ecd1ce3170b03ef741a20ea5e561f7d40a
Kevin Young <kyoung18@terpmail.umd.edu>
2013-06-08 00:33:44 -0400

    finished part 1, 2 and 3 of hw2 for CS169.1X

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,57 @@
 class MoviesController < ApplicationController
 
+
+  def index
+    @all_ratings, selected_ratings, @checked_ratings = Movie.ratings, [], Movie.ratings
+
+    if params[:sort] == "title" 
+      @movies = Movie.group("title").where(:rating => session[:ratings]).all
+      @checked_ratings = session[:ratings]
+      @hilite1 = "title"
+      session[:sort] = params[:sort]
+
+    elsif params[:sort] == "release_date"
+      @movies = Movie.group("release_date").where(:rating => session[:ratings]).all
+      @checked_ratings = session[:ratings]
+      @hilite2 = "release_date"
+      session[:sort] = params[:sort]
+
+    elsif !params[:ratings].nil?
+      params[:ratings].each_key {|k| selected_ratings << k} unless params[:ratings].nil?
+      session[:ratings] = selected_ratings
+      @movies = Movie.group(session[:sort]).where(:rating => selected_ratings).all
+      @checked_ratings = session[:ratings]
+      if session[:sort] == "title"
+        @hilite1 = "title"
+      elsif session[:sort] == "release_date"
+        @hilite2 = "release_date" 
+      end
+
+    else # No params selected, index page reloaded
+      @movies = Movie.all
+      if session[:sort] == "title"
+        @hilite1 = "title"
+        @movies = Movie.group("title").all
+      elsif session[:sort] == "release_date"
+        @hilite2 = "release_date" 
+        @movies = Movie.group("release_date").all
+      end
+      @checked_ratings = session[:ratings]
+      if !session[:sort].nil?
+        @movies = Movie.group(session[:sort]).where(:rating => session[:ratings]).all
+      else
+        @movies = Movie.where(:rating => session[:ratings]).all
+      end
+      flash.keep
+      redirect_to movies_path(:sort => session[:sort], :rating => session[:ratings].to_param)
+    end
+
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
-  end
-
-  def index
-    @movies = Movie.all
   end
 
   def new
